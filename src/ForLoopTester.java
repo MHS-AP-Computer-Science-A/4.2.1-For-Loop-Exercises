fd7de1ef6144c17eb075eafcb138c2b23e41f2e4
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-10-07 16:40:52 -0400

    Add files via upload

---
+++ src/ForLoopTester.java
@@ -0,0 +1,33 @@
+
+public class ForLoopTester {
+
+	public static void main(String[] args) {
+
+		// 1. Write a for loop that prints the odd numbers
+		//    from 1 to 20
+
+		// 2. Write a for loop that prints "Avocado"
+		//    exactly 7 times
+
+		// 3. Run this code first to see what is does.
+		//    Replace the while loop with an equivalent for loop.
+		int x = 10;
+		while (x > 0) {
+			System.out.println(x);
+			x = x - 1;
+		}
+
+		// 4. Run this code first to see what is does.
+		//    Then replace the for loop with an equivalent while loop.
+		for (int i = 1; i <= 10; i = i + 2) {
+			System.out.println(i);
+		}
+
+		// 5. Create a program using a for loop which will print out
+		//    the 4’s times table through 10.
+		//    4 * 1 = 4
+		//    4 * 2 = 8 ... and so on
+
+	}
+
+}
